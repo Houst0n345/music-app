6cf22b752532c7a9abde6d84e5b7683875d67481
Vlad Lutarevich <lutarevich95@gmail.com>
2020-05-05 23:38:58 +0300

    Updates

---
+++ precache-manifest.0ce8bc1996907978add6b464d1ab2128.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e0503c1275416be1453770c319aedec",
+    "url": "/Houst0n345/music-app/index.html"
+  },
+  {
+    "revision": "d95bb821a815ae699c6a",
+    "url": "/Houst0n345/music-app/static/css/main.46fa800e.chunk.css"
+  },
+  {
+    "revision": "59c69d28d0e506472cbb",
+    "url": "/Houst0n345/music-app/static/js/2.90fff895.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/Houst0n345/music-app/static/js/2.90fff895.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d95bb821a815ae699c6a",
+    "url": "/Houst0n345/music-app/static/js/main.30d09e17.chunk.js"
+  },
+  {
+    "revision": "87e7a43627bc1b647150",
+    "url": "/Houst0n345/music-app/static/js/runtime-main.db0e4055.js"
+  }
+]);
